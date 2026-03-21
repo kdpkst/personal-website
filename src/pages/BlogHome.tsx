05527ea33d1b6ea936e60e6607a77dbb6b1cf31d
kdpkst <jammiechen0@gmail.com>
2026-03-22 00:13:25 +1300

    update title pn Blog Home page

--- src/pages/BlogHome.tsx
+++ src/pages/BlogHome.tsx
@@ -4,7 +4,7 @@ import BlogHomeContent from '../content/pages/blog-home.mdx';
 export default function BlogHome() {
   return (
     <PageLayout
-      title="My Blog"
+      title="My Blog (TBC: Still in Dev)"
       subtitle="Thoughts on software engineering, UI design, and 3D."
       accentColor="#fd79a8"
     >
